c48d7443dea8f0d6d9363896c0ee0c06580f1c93
Thivyaprakas <thivyaprakas@gmail.com>
2025-03-02 10:48:53 +0530

    Add files via upload
    My week1_day1_assignments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+		/*version of type float with a value of 91.0f
+		2. developer of type String with a value of "Google"
+		3. isBeta of type boolean with a value of false
+		4. releaseYear of type int with a value of 2008
+		5. shortcutKey of type char with a value of 'C'*/
+
+		float version = 91.0f;
+		String dev = "Google";
+		boolean isBetatype = false;
+		int releaseyear = 2008;
+		char shortcut ='c';
+
+		System.out.println("chrome version is "+version);
+		System.out.println("chrome developer is "+dev);
+		System.out.println("is type of the chrome is Beta? "+isBetatype);
+		System.out.println("chrome releaseyear is "+releaseyear);
+		System.out.println("chrome shortcutkey is "+shortcut);
+
+
+
+	}
+
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float browserVersion = 100.2f; // (use dataType float)
+		String browserName = "firefox"; // (use dataType String)
+				boolean isVisible = true; // (use dataType boolean)
+				int releaseYear = 1998; // (use dataType int)
+				char browserLogo = 'f'; // (use datatype char)
+				
+				System.out.println("browser verion of firebox is"+browserVersion);
+				System.out.println("browser name of firebox is "+browserName);
+				System.out.println("browser visiblity of firebox is "+isVisible);
+				System.out.println("browser releaseyear of firebox is "+releaseYear);
+				System.out.println("browser browserlog of firebox is "+browserLogo);
+
+
+
+	}
+
+}
