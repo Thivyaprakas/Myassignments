eb81cd0f8264bc7eff0842bd53b8a81b45216804
Thivyaprakas <thivyaprakas@gmail.com>
2025-03-02 17:24:57 +0530

    Add files via upload
    week1.day2

---
+++ day2/FindOddNumber.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+
+
+public class FindOddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int maxnumber =1;
+		for (maxnumber=1 ; maxnumber<=10; maxnumber++) {
+			if(maxnumber%2==0) 
+			{
+				System.out.println(maxnumber);
+			
+		}
+			
+		}
+
+	}
+
+}
